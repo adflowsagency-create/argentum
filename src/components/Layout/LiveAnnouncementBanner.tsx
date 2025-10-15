9d05cd286b173e0d03bdf6268016f770b314928f
adflowsagency-create <adflowsagency@gmail.com>
2025-10-15 09:59:21 -0600

    Fix live banner overlapping with sidebar

--- src/components/Layout/LiveAnnouncementBanner.tsx
+++ src/components/Layout/LiveAnnouncementBanner.tsx
@@ -40,10 +40,10 @@ export default function LiveAnnouncementBanner({
     <div
       className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ease-out ${
         isVisible ? 'translate-y-0 opacity-100' : '-translate-y-full opacity-0'
-      }`}
+      } left-0 lg:left-64 right-0`}
     >
       <div className="bg-gradient-to-r from-green-500 via-emerald-500 to-teal-500 shadow-lg">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="px-4 sm:px-6 lg:px-8">
           <div
             className="py-3 cursor-pointer hover:bg-black/5 transition-colors duration-200 rounded-lg"
             onClick={onJoinLive}
